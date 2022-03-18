26373e0c73877fc085797836c8a5a8910ed9e192
khaled <khaledb.yahya@gmail.com>
2022-03-18 17:49:42 +0100

    fixed api updates

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let cityInput = "Moscow";
 
 cities.forEach((city) => {
     city.addEventListener('click', (e) => {
-        cityInput = e.target.textContent;
+        cityInput = e.target.innerHTML;
         fetchWeatherData();
         app.style.opcaity = "0";
     });
@@ -45,110 +45,113 @@ function dayOfTheWeek(day, month, year) {
         "Wednesday",
         "Thursday",
         "Friday",
-        "Saturday",
+        "Saturday"
     ];
+
     return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+
 };
 
-var api = `https://api.weatherapi.com/v1/current.json?key=edfdd9e1ff7a462f99104631221003&q=${cityInput}`
 
 function fetchWeatherData() {
-    fetch(api)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=edfdd9e1ff7a462f99104631221003&q=${cityInput}`)
         .then(response => response.json())
-        .then(data => { console.log(data);
+        .then(data => {
+        console.log(data);
 
-            temp.innerHTML = data.current.temp_c + "&#176;";
-            conditionOutput.innerHTML = data.current.condition.text;
-
-
-            const date = data.location.localtime;
-            const y = parseInt(date.substr(0, 4));
-            const m = parseInt(date.substr(5, 2));
-            const d = parseInt(date.substr(8, 2));
-            const time = date.substr(11);
+        temp.innerHTML = data.current.temp_c + "&#176;";
+        conditionOutput.innerHTML = data.current.condition.text;
 
 
-            dateOutput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d}, ${m}, ${y}`;
-            timeOutput.innerHTML = time;
+        const date = data.location.localtime;
+        const y = parseInt(date.substr(0, 4));
+        const m = parseInt(date.substr(5, 2));
+        const d = parseInt(date.substr(8, 2));
+        const time = date.substr(11);
 
-            nameOutput.innerHTML = data.location.name;
 
-            const iconId = data.current.condition.icon.subtr(
-                "//cdn.weatherapi.com/weather/64x64/".length);
-            icon.src = "./icons" + iconId;
 
-            cloudOutput.innerHTML = data.current.cloud + "%";
-            humidityOutput.innerHTML = data.current.humidity + "%";
-            windOutput.innerHTML = data.current.wind_kph + "km/h";
+        dateOutput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d}, ${m}, ${y}`;
+        timeOutput.innerHTML = time;
 
-            let timeOfDay = "day";
-            const code = data.current.condition.code;
+        nameOutput.innerHTML = data.location.name;
 
-            if (!data.current.is_day) {
-                timeOfDay = "night";
+        const iconId = data.current.condition.icon.subtr(
+            "//cdn.weatherapi.com/weather/64x64/".length);
+        icon.src = "./icons" + iconId;
+
+        cloudOutput.innerHTML = data.current.cloud + "%";
+        humidityOutput.innerHTML = data.current.humidity + "%";
+        windOutput.innerHTML = data.current.wind_kph + "km/h";
+
+        let timeOfDay = "day";
+        const code = data.current.condition.code;
+
+        if (!data.current.is_day) {
+            timeOfDay = "night";
+        }
+
+        if (code == 1000) {
+            app.style.backgroundImage = `
+            url(./images/${timeOfDay}/clear.jpg1)`;
+            btn.style.background = "#e5ba92";
+            if (timeOfDay == "night") {
+                btn.style.background = "#181e27";
             }
-
-            if (code == 1000) {
-                app.style.backgroundImage = `
-                url(./images/${timeOfDay}/clear.jpg1)`;
-                btn.style.background = "#e5ba92";
-                if (timeOfDay == "night") {
-                    btn.style.background = "#181e27";
-                }
-            } else if (
-                code == 1003 ||
-                code == 1006 ||
-                code == 1009 ||
-                code == 1030 ||
-                code == 1069 ||
-                code == 1087 ||
-                code == 1135 ||
-                code == 1273 ||
-                code == 1276 ||
-                code == 1279 ||
-                code == 1282
-            ) {
-                app.style.backgroundImage = `
-                url(./images/${timeOfDay}/cloudy.jpg1)`;
-                btn.style.background = "#fa6d1b";
-                if (timeOfDay == "night") {
-                    btn.style.background = "#181e27";
-                }
-            } else if (
-                code == 1063 ||
-                code == 1069 ||
-                code == 1072 ||
-                code == 1150 ||
-                code == 1153 ||
-                code == 1180 ||
-                code == 1183 ||
-                code == 1186 ||
-                code == 1189 ||
-                code == 1192 ||
-                code == 1195 ||
-                code == 1204 ||
-                code == 1207 ||
-                code == 1240 ||
-                code == 1243 ||
-                code == 1246 ||
-                code == 1249 ||
-                code == 1252
-            ) {
-                app.style.backgroundImage = `
-                url(./images/${timeOfDay}/rainy.jpg1)`;
-                btn.style.background = "#fa6d1b";
-                if (timeOfDay == "night") {
-                    btn.style.background = "#325c80";
-                }
-            } else {
-                app.style.backgroundImage = `
-                url(./images/${timeOfDay}/snowy.jpg1)`;
-                btn.style.background = "#4d72aa";
-                if (timeOfDay == "night") {
-                    btn.style.background = "#1b1b1b";
-                }
+        } else if (
+            code == 1003 ||
+            code == 1006 ||
+            code == 1009 ||
+            code == 1030 ||
+            code == 1069 ||
+            code == 1087 ||
+            code == 1135 ||
+            code == 1273 ||
+            code == 1276 ||
+            code == 1279 ||
+            code == 1282
+        ) {
+            app.style.backgroundImage = `
+            url(./images/${timeOfDay}/cloudy.jpg1)`;
+            btn.style.background = "#fa6d1b";
+            if (timeOfDay == "night") {
+                btn.style.background = "#181e27";
             }
-            app.style.opcaity = "1";
+        } else if (
+            code == 1063 ||
+            code == 1069 ||
+            code == 1072 ||
+            code == 1150 ||
+            code == 1153 ||
+            code == 1180 ||
+            code == 1183 ||
+            code == 1186 ||
+            code == 1189 ||
+            code == 1192 ||
+            code == 1195 ||
+            code == 1204 ||
+            code == 1207 ||
+            code == 1240 ||
+            code == 1243 ||
+            code == 1246 ||
+            code == 1249 ||
+            code == 1252
+        ) {
+            app.style.backgroundImage = `
+            url(./images/${timeOfDay}/rainy.jpg1)`;
+            btn.style.background = "#fa6d1b";
+            if (timeOfDay == "night") {
+                btn.style.background = "#325c80";
+            }
+        } else {
+            app.style.backgroundImage = `
+            url(./images/${timeOfDay}/snowy.jpg1)`;
+            btn.style.background = "#4d72aa";
+            if (timeOfDay == "night") {
+                btn.style.background = "#1b1b1b";
+            }
+        }
+        app.style.opcaity = "1";
 
         })
 
